b5ab7764ff2b8a1a0bdc8024fbc0e205f2b4e4b0
PeterSchmiedtGreencode <peter.schmiedt@greencode.cz>
2024-04-22 16:05:44 +0200

    Initial Commit

---
+++ mdevcamp-greencode/mdevcamp_greencodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mdevcamp_greencodeApp.swift
+//  mdevcamp-greencode
+//
+//  Created by Peter Schmiedt on 22.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct mdevcamp_greencodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
